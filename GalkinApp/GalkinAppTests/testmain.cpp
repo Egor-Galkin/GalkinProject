0a145e58e4d97373cc52f5150f319e3d8f9e046c
Egor-Galkin <egorgalki4@gmail.com>
2025-07-14 19:13:02 +0300

    Delete previous version

--- GalkinApp/GalkinAppTests/testmain.cpp
+++
@@ -1,9 +0,0 @@
-#include <QCoreApplication>
-#include "../testsearchengine.h"
-
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-    TestSearchEngine test;
-    return QTest::qExec(&test, argc, argv);
-}
